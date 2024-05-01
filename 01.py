c406e2b92f49aa116a3140cfa8a14b0fc18b1c45
DimasWerito <krasilnykov@gmail.com>
2024-04-30 21:34:32 -0600

    day 1

--- 01.py
+++ 01.py
@@ -2,9 +2,48 @@
 Big O: O(n)
 """
 
-
-def print_items(n):
+"""
+def print_items(n): # O(n)
     for i in range(n):
         print(i)
 
 print_items(10)
+"""
+
+"""
+def print_items(n): 2O(n)
+    for i in range(n):
+        print(i)
+    for j in range(n):
+        print(j)
+
+print_items(10)
+"""
+
+"""def print_items(n): # O(n*n)
+    for i in range(n):
+        for j in range(n):
+            print(i, j)
+
+print_items(10)"""
+
+"""def print_items(n): # O(n*n+n)
+    for i in range(n):
+        for j in range(n):
+            print(i, j)
+    for k in range(n):
+        print(k)
+
+print_items(10)"""
+
+"""def add_items(n): # O(1)
+    return n + n"""
+
+"""def print_items(a, b): # O(a + b)
+    for i in range(a):
+        print(i)
+    for j in range(b):
+        print(j) 
+
+print_items(10, 10)"""
+
